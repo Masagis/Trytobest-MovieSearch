3d00be171bd26917dec8943ecfdf6d0a2f6a1a43
Agis Tri Wahyuji <agis.14115041@student.itera.ac.id>
2020-09-28 17:41:32 +0700

    Revisi error scss

--- webpack.common.js
+++ webpack.common.js
@@ -1,73 +1,63 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: [
-    "./src/app.js",
-    "font-awesome/scss/font-awesome.scss"
-  ],
+  entry: ['./src/app.js', 'font-awesome/scss/font-awesome.scss'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [{
         test: /\.(css|scss)$/,
         use: [{
-            loader: 'style-loader'
+            loader: 'style-loader', // inject CSS to page
           },
           {
-            loader: 'css-loader',
+            loader: 'css-loader', // translates CSS into CommonJS modules
           },
           {
-            loader: 'sass-loader'
+            loader: 'postcss-loader', // Run postcss actions
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }
-        ]
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf)$/i,
-        use: 'url-loader?limit=1024&name=assets/fonts/[name].[ext]'
+        use: 'url-loader?limit=1024&name=assets/fonts/[name].[ext]',
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           'file-loader?name=assets/images/[name].[ext]',
-          'image-webpack-loader?bypassOnDebug'
-        ]
+          'image-webpack-loader?bypassOnDebug',
+        ],
       },
       {
         test: /font-awesome\.config\.js/,
         use: [{
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
-            loader: 'font-awesome-loader'
-          }
-        ]
+            loader: 'font-awesome-loader',
+          },
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "./src/assets/images/fav-icon.png",
-      template: "./src/template.html",
-      filename: "index.html",
-      minify: "production"
+      favicon: './src/assets/images/fav-icon.png',
+      template: './src/template.html',
+      filename: 'index.html',
+      minify: 'production',
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
+      jQuery: 'jquery',
     }),
   ],
 };
